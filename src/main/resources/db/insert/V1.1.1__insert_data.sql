e5a54328bb320a430cda69300c9de65256526580
zzborowska <zuzannazborowska@gmail.com>
2020-05-11 12:42:35 +0200

    public schema

--- src/main/resources/db/insert/V1.1.1__insert_data.sql
+++ src/main/resources/db/insert/V1.1.1__insert_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO users.profile_details
+INSERT INTO public.profile_details
 VALUES (3455, 'My first profile on Psinder', 'picture_id');
 
-INSERT INTO users.profile
+INSERT INTO public.profile
 VALUES (1, 'psiaczek96', 123, now(), 3455);
