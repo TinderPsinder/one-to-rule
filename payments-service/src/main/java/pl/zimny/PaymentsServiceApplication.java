0a26f7d864c4f619f5749b03dba17d9a6278da9c
zzborowska <zuzannazborowska@gmail.com>
2020-05-18 21:17:52 +0200

    enable discovery client

--- payments-service/src/main/java/pl/zimny/PaymentsServiceApplication.java
+++ payments-service/src/main/java/pl/zimny/PaymentsServiceApplication.java
@@ -2,8 +2,10 @@ package pl.zimny;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentsServiceApplication {
 
     public static void main(String[] args) {
